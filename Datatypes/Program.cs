26ab9c59f0bfc7e6520bd424cf6b303d3fadff17
Lemi02 <36259352+Lemi02@users.noreply.github.com>
2021-01-31 15:10:47 +0100

    Naamgevingen aangepast aan conventies

--- Datatypes/Program.cs
+++ Datatypes/Program.cs
@@ -11,42 +11,42 @@ namespace Datatypes
         static void Main(string[] args)
         {
             //Er is geen verschil tussen String en string
-            String eersteString = "Eerste";
-            string tweedeString = "Tweede";
-            var derdeString = "derde";
+            String EersteString = "Eerste";
+            string TweedeString = "Tweede";
+            var DerdeString = "derde";
 
-            char eersteChar = 'A';
-            var tweedeChar = 'B';
+            char EersteChar = 'A';
+            var TweedeChar = 'B';
 
-            int eersteInt = 5;
-            var tweedeInt = 6;
+            int EersteInt = 5;
+            var TweedeInt = 6;
 
             //maximale waarde van de int, int is zowel positief als negatief(signed)
-            int groteInt = int.MaxValue;
-            Console.WriteLine(groteInt.ToString());
+            int GroteInt = int.MaxValue;
+            Console.WriteLine(GroteInt.ToString());
             //maximale waarde van de unsigned int.
-            uint grotePositieveInt = uint.MaxValue;
-            Console.WriteLine(grotePositieveInt.ToString());
+            uint GrotePositieveInt = uint.MaxValue;
+            Console.WriteLine(GrotePositieveInt.ToString());
 
-            short kleinerGetal = short.MaxValue;
-            Console.WriteLine(kleinerGetal.ToString());
+            short KleinerGetal = short.MaxValue;
+            Console.WriteLine(KleinerGetal.ToString());
 
             //voor echt grote getallen kan je ook nog de ulong gebruiken(unsigned)
-            long groterGetal = long.MaxValue;
-            Console.WriteLine(groterGetal.ToString());
+            long GroterGetal = long.MaxValue;
+            Console.WriteLine(GroterGetal.ToString());
 
             //een kommagetal wil C# automatisch in een double plaatsen. Je kan een float forceren door een f achter de kommawaarde te zetten
-            float kommaGetal = 3.14f;
-            var kommaGetal2 = 3.14f;
+            float KommaGetal = 3.14f;
+            var KommaGetal2 = 3.14f;
 
             //in principe wordt bij de var van een kommagetal automatisch een double gemaakt voor 100% zekerheid plaats je een d erachter
-            double kommaGetal3 = 3.14;
-            var kommaGetal4 = 3.14d;
+            double KommaGetal3 = 3.14;
+            var KommaGetal4 = 3.14d;
 
             // een aantal manieren om een boolean te definieren
-            bool waarOfNietWaar = true;
-            var nietWaarOfWaar = false;
-            Boolean waarOfNiet = true;
+            bool WaarOfNietWaar = true;
+            var NietWaarOfWaar = false;
+            Boolean WaarOfNiet = true;
 
 
 
